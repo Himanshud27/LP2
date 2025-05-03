067f7a3e107a537ad91f40db947bf79c91bda52f
Himanshud27 <dhandehimanshu148@gmail.com>
2025-05-03 13:40:44 +0530

    practise nqueen update

--- practise/nqueen.py
+++ practise/nqueen.py
@@ -1,46 +1,45 @@
 def main():
-    n = int(input("Enter the number of Queen : "))
-    board = [[0 for _ in range(n)]for _ in range(n)]
-    solution = []
+    n = int(input("Enter the number Queens : "))
+    grid = [[0 for _ in range (n)] for _ in range (n)]
+    Solution = []
+    nQueen(grid,Solution,0,n)
+    j = 1
+    for i in Solution:
+        print(f"Solution {j}")
+        j = j +1
+        for k in i:
+            print(k)
 
-    nqueen(board,0,n,solution)
-    print(solution)
 
 
-def nqueen(board,col,n,solution):
-    if col==n:
-        solution.append([row[:] for row in board])
+
+def nQueen(grid,solutions,col,n):
+    if col == n:
+        solutions.append([row[:] for row in grid])  
         return
     
-    for i in range(n):
-        if issafe(board,i,col,n):
-            board[i][col] = 1
-            nqueen(board,col+1,n,solution)
-            board[i][col] = 0
+    for i  in range(n):
+        if issafe(grid,i,col,n):
+          grid[i][col]=1
+          nQueen(grid,solutions,col+1,n)
+          grid[i][col]=0
 
 
-
-
-
-
-def issafe(board,row,col,n):
+def issafe(grid,row,col,n):
     for i in range (col):
-        if board[row][i] == 1 :
+        if grid[row][i] == 1:
             return False
-
+        
     for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
-        if board[i][j] == 1:
+        if grid[i][j]==1:
             return False
 
     for i,j in zip(range(row,n),range(col,-1,-1)):
-        if board[i][j] == 1:
+        if grid[i][j]==1:
             return False
 
-    return True    
-
-   
+    return True       
 
 
 if __name__ == "__main__":
     main()
-    
